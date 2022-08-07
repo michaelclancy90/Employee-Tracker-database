678124f826550c86c7dd4bf2a4156162a6b0e455
michaelclancy90 <michaelclancy@hotmail.co.uk>
2022-08-07 16:45:03 +1000

    package variables set up, port set up. .env details added

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+require('console.table');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER, 
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    },
+);
