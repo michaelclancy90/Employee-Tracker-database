c6ec3f672d2b55397fd53bc3687c546289fd06fe
michaelclancy90 <michaelclancy@hotmail.co.uk>
2022-08-07 19:01:29 +1000

    question and functions set out in server.js

--- server.js
+++ server.js
@@ -18,3 +18,38 @@ const db = mysql.createConnection(
         database: process.env.DB_NAME
     },
 );
+
+function init () {
+    inquirer
+    .prompt(
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: [
+            'View All Employess',
+            'Add Employee',
+            'Update Employee Role',
+            'View All Roles',
+            'Add Role',
+            'View All Departments',
+            'Add Departments',
+            'Exit'
+        ],
+        }
+    )
+}
+
+viewEmployee()
+
+addEmployee()
+
+updateEmployeeRole()
+
+viewAllRoles()
+
+viewAllDepartments()
+
+addDepartment()
+
+init ()
